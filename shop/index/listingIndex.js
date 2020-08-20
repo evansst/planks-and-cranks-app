6b8a9e57c8ae264fa7aa994f8515ad96120a8116
Sam Evans <evansst@icloud.com>
2020-08-20 11:28:41 -0600

    fixes padding on listing index

--- shop/index/listingIndex.js
+++ shop/index/listingIndex.js
@@ -11,7 +11,7 @@ export default function listingIndex() {
 
 function displayCards(listings) {
   const $cardDeck = document.createElement('div');
-  $cardDeck.className = 'row row-cols-1 row-cols-md-3 pt-5';
+  $cardDeck.className = 'row row-cols-1 row-cols-md-3 p-5';
 
   listings
     .map(listing => toListingCard(listing)($cardDeck));
